d2fb6d77249dc3daa0bd2509e341e9a1927ff316
stoopwaffel <stoopwaffel@gmail.com>
2021-10-27 14:50:23 -0400

    second commit

--- contracts/DCAOperator.sol
+++ contracts/DCAOperator.sol
@@ -2,6 +2,39 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DCAOperator {
+
+  enum SupportedTokens {
+    WBTC,
+    WETH
+  }
+
+  enum DepositTokens {
+    USDC,
+    DAI,
+    USDT,
+    GUSD
+  }
+
+  struct Instructions {
+    SupportedTokens tokenToBuy;
+    DepositTokens tokenToDeposit;
+    uint amountInUSD;
+  }
+
+  mapping(address => Instructions) userInstructions;
   constructor() public {
+  }
+
+  function registerUser(address userAddress) public {
+    // adds user address to the mapping state variable
+    // also adds the cadence and amount and token
+  }
+
+  function deposit() payable public {
+    // deposit funds to the contract
+  }
+
+  function tradeDepositTokensForSupportedTokens() private {
+    // trade with some provider
   }
 }
